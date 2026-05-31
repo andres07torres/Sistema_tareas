14a3478d15f632ad4d8cedd34fbd4d274afb39fb
andres07torres <andres07torresg@gmail.com>
2026-05-31 15:28:50 -0500

    [ADD]

--- public/materias.php
+++ public/materias.php
@@ -143,6 +143,26 @@ $materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
         .modal-form input[type="date"] {
             -webkit-appearance: none;
             appearance: none;
+            position: relative;
+            background-color: white;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%2365676b' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Crect width='18' height='18' x='3' y='4' rx='2' ry='2'%3E%3C/rect%3E%3Cline x1='16' x2='16' y1='2' y2='6'%3E%3C/line%3E%3Cline x1='8' x2='8' y1='2' y2='6'%3E%3C/line%3E%3Cline x1='3' x2='21' y1='10' y2='10'%3E%3C/line%3E%3C/svg%3E");
+            background-repeat: no-repeat;
+            background-position: right 1rem center;
+            background-size: 1.25rem;
+            padding-right: 2.5rem;
+            text-align: left;
+        }
+
+        .modal-form input[type="date"]::-webkit-calendar-picker-indicator {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            cursor: pointer;
         }
 
         .modal-form input:focus, .modal-form select:focus, .modal-form textarea:focus {
